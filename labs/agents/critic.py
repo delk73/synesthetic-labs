0b11baaa6df2573e93331b030996f263fec167a3
Dan Elkins <dan.elkins@gmail.com>
2025-10-06 22:19:06 -0700

    ci

--- labs/agents/critic.py
+++ labs/agents/critic.py
@@ -146,8 +146,6 @@ class CriticAgent:
                         self._logger.error(message)
                         should_attempt_validation = False
                     else:
-                        issues.pop()
-                        issues.pop()
                         validation_status = "degraded"
                         validation_reason = message
                         validation_error = _build_error_payload(str(exc))
@@ -177,8 +175,6 @@ class CriticAgent:
                     validation_reason = message
                     self._logger.error(message)
                 else:
-                    issues.pop()
-                    issues.pop()
                     validation_status = "degraded"
                     validation_reason = message
                     validation_error = _build_error_payload(str(exc))
